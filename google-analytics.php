d4eb7cdddd4be6ae9bf7272168414cd6300944bd
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-09-19 15:30:36 +0200

    Removed acf

--- google-analytics.php
+++ google-analytics.php
@@ -30,17 +30,6 @@ if (file_exists(GOOGLEANALYTICS_PATH . 'vendor/autoload.php')) {
 	require_once GOOGLEANALYTICS_PATH . 'vendor/autoload.php';
 }
 
-// Acf auto import and export
-add_action('plugins_loaded', function () {
-    $acfExportManager = new \AcfExportManager\AcfExportManager();
-    $acfExportManager->setTextdomain('google-analytics');
-    $acfExportManager->setExportFolder(GOOGLEANALYTICS_PATH . 'acf-fields/');
-    $acfExportManager->autoExport(array(
-        'google-analytics' => 'group_56c5c4d919d29'
-    ));
-    $acfExportManager->import();
-});
-
 // Instantiate and register the autoloader
 $loader = new GoogleAnalytics\Vendor\Psr4ClassLoader();
 $loader->addPrefix('GoogleAnalytics', GOOGLEANALYTICS_PATH);
